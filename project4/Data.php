296c988bd7413f9f6dac8b588f4c5370d9de7d8f
DavidLewis8826 <70353964+DavidLewis8826@users.noreply.github.com>
2024-04-26 13:44:42 -0400

    1

--- project4/Data.php
+++ project4/Data.php
@@ -4,7 +4,6 @@
 <head>
     <title>Project 3 - Home</title>
     <link rel="stylesheet" href="css.css">
-    <script src="project3.js"></script>
     <meta charset="UTF-8">
     <meta name="description" content="Project 3 home for David Lewis CMST 386 project. Topics include responsive design and targeting your audience">
     <meta name="keywords" content="Project 3, David Lewis, responsive design, targeting audience">
@@ -12,33 +11,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-        <h1>CMST 386 - Project 3</h1>
-        <table> 
-            <tr> 
-              <th>Name</th> 
-              <th>Email</th> 
-              <th>Phone</th> 
-            </tr> 
-            <?php 
-              $file = fopen("list.csv", "r"); 
-              while (!feof($file)) { 
-                $row = fgetcsv($file); 
-                if ($row) { 
-                  echo "<tr>"; 
-                  echo "<td>" . $row[0] . "</td>"; 
-                  echo "<td>" . $row[1] . "</td>"; 
-                  echo "<td>" . $row[2] . "</td>"; 
-                  echo "</tr>"; 
-                } 
-              } 
-              fclose($file); 
-            ?> 
-          </table> 
-        <div class="flex-container">
-            <div>
-                
-            </div>
-        </div>
+  <h1>CMST 386 - Project 3</h1>
+  <div class="main">
+    <div class="flex-container">
+      <div>
+      <table> 
+        <tr> 
+          <th>Name</th> 
+          <th>Email</th> 
+          <th>Phone</th> 
+          </tr> 
+          <?php 
+          $file = fopen("list.csv", "r"); 
+          while (!feof($file)) { 
+          $row = fgetcsv($file); 
+          if ($row) { 
+            echo "<tr>"; 
+            echo "<td>" . $row[0] . "</td>"; 
+            echo "<td>" . $row[1] . "</td>"; 
+            echo "<td>" . $row[2] . "</td>"; 
+            echo "</tr>"; 
+            } 
+          } 
+          fclose($file); 
+          ?> 
+      </table> 
+      </div>
+    </div>
     </div>
     <div class="footer">
         <p>Author: David Lewis</p>
