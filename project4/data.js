80cb1e1f649a919f02f3fe6dd0aa6a82962a834e
DavidLewis8826 <70353964+DavidLewis8826@users.noreply.github.com>
2024-04-26 21:21:25 +0000

    1

--- project4/data.js
+++ project4/data.js
@@ -4,15 +4,25 @@ function demoA () {
       download: true,
       skipEmptyLines: true,
   
-      // (PART B) DRAW CSV FILE
-      complete : csv => {
+         // (PART B) DRAW CSV FILE
+    complete : csv => {
         // (B1) GET + RESET HTML TABLE
-        var table = document.getElementById("demoA");
+        var table = document.getElementById("demoC");
         table.innerHTML = "";
   
-        // (B2) DRAW TABLE ROWS
-        for (let row of csv.data) {
-          let tr = table.insertRow();
+        // (B2) DRAW TABLE HEADER
+        var thead = table.createTHead(),
+            tr = thead.insertRow();
+        for (let cell of csv.data[0]) {
+          let td = tr.insertCell();
+          td.innerHTML = cell;
+        }
+  
+        // (B3) DRAW TABLE BODY
+        var tbody = table.createTBody();
+        for (let i=1; i<csv.data.length; i++) {
+          let row = csv.data[i],
+              tr = tbody.insertRow();
           for (let cell of row) {
             let td = tr.insertCell();
             td.innerHTML = cell;
